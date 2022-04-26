bbe5b189ca4221a3c983d93652e20693b89f5568
Manuel Schrauth <manuel.schrauth@live.de>
2022-04-26 14:20:20 +0200

    more comments

--- hypertiling/core.py
+++ hypertiling/core.py
@@ -13,11 +13,10 @@ class HyperbolicTiling:
         self.p = p  # number of edges (and thus number of vertices) per polygon
         self.q = q  # number of polygons that meet at each vertex
         self.nlayers = nlayers  # layers of the tessellation
-        self.nsectors = 360
 
         self.phi = 2*np.pi/self.p  # angle of rotation that leaves the lattice invariant
         self.degphi = 360/self.p
-        self.dgts = 8  # rounding digits, default: 8 (do not change, unless you know what you are doing!)
+        self.dgts = 8   # rounding digits, default: 8 (do not change, unless you know what you are doing!)
         self.degtol = 1 # sector boundary tolerance during lattice construction
 
         self.centerlist = []  # used to keep track of which polygons has already been drawn
@@ -55,20 +54,24 @@ class HyperbolicTiling:
             polygon.verticesW[:, i] = p2w(z)
         return polygon
 
-    # generates the whole lattice by first constructing one 1/p sector, then uses symmetry to construct
-    # the other p-1 sectors
+
+
+    # generates the whole lattice by first constructing one 1/p sector, 
+    # then uses symmetry to construct the other p-1 sectors
+
+    # in order to avoid problems associated to rounding we construct the
+    # fundamental sector a little bit wider than 360/p degrees in filter
+    # out rotational duplicates after all layers have been constructed
+
     def generate(self):
         # prepare list to store polygons 
         self.lpolygons[0].append(self.fund_poly)
 
-        # prepare set which will contain the center coordinates
-        # this is being used for uniqueness checks
+        # prepare sets which will contain the center coordinates
+        # this is used for uniqueness checks later
         centerset = set()
         centerset_extra = set()
         centerset.add(np.round(self.fund_poly.centerP, self.dgts))
-
-        # include one extra sector as "buffer"
-        # sectors = np.arange(0, self.nsectors + 1)
 
         # loop over layers to be constructed
         for l in range(1, self.nlayers):
@@ -112,12 +115,10 @@ class HyperbolicTiling:
                                 centerset_extra.add(center)
 
 
-
         # flatten the list
         for curr_layer in self.lpolygons:
             for polygon in curr_layer:
                 self.polygons.append(polygon)
-
 
 
         # filter out rotational duplicates
@@ -134,9 +135,10 @@ class HyperbolicTiling:
         self.polygons = list(np.delete(self.polygons, deletelist))
             
 
-
-        # uses symmetry to fill the disk by rotating the slice
+        # fill entire disk by rotating the slice
         self.angular_replicate(copy.deepcopy(self.polygons), self.p)
+
+
 
 
     # finds the next polygon by k-fold rotation of polygon around the vertex number ind
