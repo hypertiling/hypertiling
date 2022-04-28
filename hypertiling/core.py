eb93fd48f4473278d82339803452ea88dd4bd28e
Manuel Schrauth <manuel.schrauth@live.de>
2022-04-28 11:51:01 +0200

    minor fixes

--- hypertiling/core.py
+++ hypertiling/core.py
@@ -140,7 +140,6 @@ class HyperbolicTiling:
 
 
 
-
     # finds the next polygon by k-fold rotation of polygon around the vertex number ind
     def generate_adj_poly(self, polygon, ind, k):
         z0 = polygon.verticesP[ind]
@@ -157,7 +156,8 @@ class HyperbolicTiling:
             for polygon in polygons:
                 pgon = copy.deepcopy(polygon)
                 pgon.moeb_rotate(-p*self.phi)
-                pgon.find_angle()  # set polygon.sector such that the disk is divided into 1*p sectors
+                pgon.find_angle()
+                pgon.find_sector()
                 self.polygons.append(pgon)
 
         # assign each polygon a unique number
