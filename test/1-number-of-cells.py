0d32998367337db6f0e30921fe843549bce0b460
Manuel Schrauth <manuel.schrauth@live.de>
2022-05-12 01:59:24 +0200

    first attempt

---
+++ test/1-number-of-cells.py
@@ -0,0 +1,87 @@
+import unittest
+from hypertiling import HyperbolicTiling
+from hypertiling.util import n_cell_centered
+
+
+
+class TestNumberOfCells(unittest.TestCase):
+    def test_num_cells_cell_centered(self):
+        p, q = 3, 7
+        for n in range(5):
+            T = HyperbolicTiling(p, q, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(p,q,n),len(T))
+
+            T = HyperbolicTiling(q, p, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(q,p,n),len(T))
+
+        p, q = 3, 8
+        for n in range(5):
+            T = HyperbolicTiling(p, q, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(p,q,n),len(T))
+
+            T = HyperbolicTiling(q, p, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(q,p,n),len(T))
+
+        p, q = 4, 5
+        for n in range(5):
+            T = HyperbolicTiling(p, q, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(p,q,n),len(T))
+
+            T = HyperbolicTiling(q, p, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(q,p,n),len(T))
+
+        p, q = 4, 6
+        for n in range(5):
+            T = HyperbolicTiling(p, q, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(p,q,n),len(T))
+
+            T = HyperbolicTiling(q, p, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(q,p,n),len(T))
+
+        p, q = 4, 7
+        for n in range(5):
+            T = HyperbolicTiling(p, q, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(p,q,n),len(T))
+
+            T = HyperbolicTiling(q, p, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(q,p,n),len(T))
+
+        p, q = 4, 8
+        for n in range(5):
+            T = HyperbolicTiling(p, q, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(p,q,n),len(T))
+
+            T = HyperbolicTiling(q, p, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(q,p,n),len(T))
+
+        p, q = 5, 5
+        for n in range(5):
+            T = HyperbolicTiling(p, q, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(p,q,n),len(T))
+
+            T = HyperbolicTiling(q, p, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(q,p,n),len(T))
+
+        p, q = 5, 6
+        for n in range(5):
+            T = HyperbolicTiling(p, q, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(p,q,n),len(T))
+
+            T = HyperbolicTiling(q, p, n)
+            T.generate()
+            self.assertEqual(n_cell_centered(q,p,n),len(T))
