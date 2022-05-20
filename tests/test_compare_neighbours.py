c118067aec84ff8c803b54863af23e175595c26d
Manuel Schrauth <manuel.schrauth@live.de>
2022-05-20 08:01:51 +0200

    expand test to include brute force algorithm

--- tests/test_compare_neighbours.py
+++ tests/test_compare_neighbours.py
@@ -3,14 +3,14 @@ from hypertiling import HyperbolicTiling
 from hypertiling.neighbours import find
 
 
-kernel = "flo"
-
-lattices = [(3,7,7,), (7,3,7), (5,4,6), (4,5,6), (9,3,4), (4,10,3), (3,8,4), (6,4,4)]
-
 print("Testing different neighbour search algorithms against each other")
 
 class TestCompareNeighbours(unittest.TestCase):
     def test_comp_nbrs(self):
+
+        kernel = "flo"
+
+        lattices = [(3,7,7,), (7,3,7), (5,4,6), (4,5,6), (9,3,4), (4,10,3), (3,8,4), (6,4,4)]
 
         for p, q, nlayer in lattices:
             print("Constructing", p, q, nlayer, "lattice")
@@ -22,6 +22,19 @@ class TestCompareNeighbours(unittest.TestCase):
             
             self.assertTrue(nbrs1 == nbrs2)
 
+        kernel = "flo"
+
+        lattices = [(3,7,4), (7,3,4), (5,4,4), (4,5,4), (9,3,4), (4,10,3), (3,8,4), (6,4,4)]
+
+        for p, q, nlayer in lattices:
+            print("Constructing", p, q, nlayer, "lattice")
+            T = HyperbolicTiling(p, q, nlayer, kernel=kernel, center="cell")
+            T.generate()
+
+            nbrs1 = find(T, which="optimized")
+            nbrs2 = find(T, which="brute_force")
+            
+            self.assertTrue(nbrs1 == nbrs2)
 
 if __name__ == '__main__':
     unittest.main()
