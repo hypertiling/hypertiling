6be5b3c3a51958d2f10511d1b07829b78b3890ef
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2022-05-24 17:12:59 +0200

    FINALLY FIX WARNING....

--- hypertiling/transformation.py
+++ hypertiling/transformation.py
@@ -4,16 +4,18 @@ from numpy import array as nparray
 def ddkahan(x, y):
     """
     Transform the addition of two floating point numbers:
+
     .. math::
+
         x + y = r + e
-        
-        
+
+
     (Dekker1971) showed that this transform is exact, if abs(x) > abs(y).
-        
+
     Parameters:
         x (float): a floating point number.
         y (float): a floating point number with abs(y) < abs(x).
-        
+
     Returns:
         r (float): x + y
         e (float): the overflow
