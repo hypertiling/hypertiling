4e8478329cf64d620cb94e736ca6f39a7e504af9
Yanick Thurn <50019628+YanickT@users.noreply.github.com>
2022-08-11 14:19:50 +0200

    update-tests
    - Add until test class
    - started implementing tests for the kernel

---
+++ tests/test_kernelreflection_numba.py
@@ -0,0 +1,63 @@
+import unittest
+from tests.test_util import *
+
+
+class TestReflectTiling(unittest.TestCase):
+    def test_generate(self):
+        self.fail()
+
+    def test__get_reflection_level_in_sector(self):
+        self.fail()
+
+    def test_get_layer(self):
+        self.fail()
+
+    def test_get_sector(self):
+        self.fail()
+
+    def test_get_center(self):
+        self.fail()
+
+    def test_get_vertices(self):
+        self.fail()
+
+    def test_get_angle(self):
+        self.fail()
+
+    def test_find(self):
+        self.fail()
+
+    def test_map_layers(self):
+        self.fail()
+
+    def test__polygen(self):
+        self.fail()
+
+    def test_get_neighbors(self):
+        self.fail()
+
+    def test_get_neighbors_fast(self):
+        self.fail()
+
+    def test_check_integrity(self):
+        with PrintTest() as stream:
+            print("test_output")
+            print(123)
+            values = stream.get()
+
+        print("Outside now:")
+        print(values)
+        self.fail()
+
+    def test_transform(self):
+        self.fail()
+
+    def test_rotate(self):
+        self.fail()
+
+    def test_translate(self):
+        self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_util.py
@@ -0,0 +1,37 @@
+import io
+import sys
+
+
+class PrintTest:
+
+    """
+    Helper class for the unit tests.
+    Can be used to evaluate print-statements
+    """
+
+    def __init__(self):
+        self._capturedOutput = io.StringIO()
+
+    def __enter__(self):
+        sys.stdout = self._capturedOutput
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = sys.__stdout__
+
+    def get(self):
+        """
+        Returns all printed strings
+        return: str = string of all prints
+        """
+        return self._capturedOutput.getvalue()
+
+
+if __name__ == "__main__":
+    with PrintTest() as stream:
+        print("test_output")
+        print(123)
+        values = stream.get()
+
+    print("Outside now:")
+    print(values)
