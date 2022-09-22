72cac3065e1fc5b8447ba62d80e49eabe2abdde3
Yanick Thurn <50019628+YanickT@users.noreply.github.com>
2022-09-22 23:34:16 +0200

    update
    - add **kwargs argument to the HyperbolicTiling factory pattern
    - update test of reflection kernel to use factory pattern

--- tests/test_reflectionkernel.py
+++ tests/test_reflectionkernel.py
@@ -1,6 +1,6 @@
 import unittest
 from tests.test_util import *
-from hypertiling.generative.reflectionkernel import KernelGenerativeReflection
+from hypertiling.core import HyperbolicTiling
 import hypertiling.geodesics as geos
 import hypertiling.generative.reflectionkernel_util as util
 import random
@@ -32,14 +32,14 @@ class TestReflectTiling(unittest.TestCase):
 
     def test_generate(self):
         for combi in Progress(COMBIS):
-            tiling = KernelGenerativeReflection(combi[0], combi[1], n=MAXLAYERS)
+            tiling = HyperbolicTiling(combi[0], combi[1], n=MAXLAYERS, kernel="GRK")
             # this is basically the only test we can do and it only will scream when duplicates are found
             with PrintTest() as stream:
                 tiling.check_integrity()
 
     def test_find(self):
         for combi in Progress(COMBIS):
-            tiling = KernelGenerativeReflection(*combi)
+            tiling = HyperbolicTiling(*combi, kernel="GRK")
 
             for index in range(tiling.length - 1):
                 center = tiling[index][0]
@@ -63,7 +63,7 @@ class TestReflectTiling(unittest.TestCase):
 
     def test_get_neighbors(self):
         for combi in Progress(COMBIS):
-            tiling = KernelGenerativeReflection(*combi)
+            tiling = HyperbolicTiling(*combi, kernel="GRK")
             for index in range(tiling.length):
                 neighbors = tiling.get_neighbors(index)
 
@@ -104,7 +104,7 @@ class TestReflectTiling(unittest.TestCase):
 
     def test_get_neighbors_mapping(self):
         for combi in Progress(COMBIS):
-            tiling = KernelGenerativeReflection(*combi)
+            tiling = HyperbolicTiling(*combi, kernel="GRK")
             tiling.map_neighbors()
             for index in range(tiling.length):
                 neighbors = tiling.get_neighbors(index)
@@ -113,7 +113,7 @@ class TestReflectTiling(unittest.TestCase):
 
     def test_get_neighbors_list(self):
         for combi in Progress(COMBIS):
-            tiling = KernelGenerativeReflection(*combi)
+            tiling = HyperbolicTiling(*combi, kernel="GRK")
             neighbors_list = tiling.get_neighbors_list()  # calls tiling.map_neighbors
             for index in range(tiling.length):
                 neighbors = neighbors_list[index]
@@ -122,7 +122,7 @@ class TestReflectTiling(unittest.TestCase):
 
     def test_check_integrity(self):
         for combi in Progress(COMBIS):
-            tiling = KernelGenerativeReflection(*combi)
+            tiling = HyperbolicTiling(*combi, kernel="GRK")
 
             # check for duplicates
             old = tiling._sector_polys[-1, 0]
@@ -179,5 +179,5 @@ class TestReflectTiling(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    tiling = KernelGenerativeReflection(7, 3, 2)
+    tiling = HyperbolicTiling(7, 3, 2, kernel="GRK")
     unittest.main()
