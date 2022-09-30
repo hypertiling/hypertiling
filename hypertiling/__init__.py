10d91794b6b34f8d0810a851abb3d0817c19bb90
Yanick Thurn <50019628+YanickT@users.noreply.github.com>
2022-09-30 23:27:46 +0200

    Update __init__.py

--- hypertiling/__init__.py
+++ hypertiling/__init__.py
@@ -6,3 +6,15 @@ __version__ = "1.0.2"
 __author__ = 'Manuel Schrauth, Felix Dusel, Florian Goth, Dietmar Herdt, Jefferson S. E. Portela, Yanick Thurn'
 __credits__ = 'Institute for Theoretical Physics and Astrophysics, University of Wuerzburg'
 __packages__ = []
+
+
+# for njiting the functions
+import sys
+import io
+
+__std_out = sys.stdout
+sys.stdout = io.StringIO()
+
+tiling = HyperbolicTiling(3, 7, 2, kernel="GR")
+
+sys.stdout = __std_out
