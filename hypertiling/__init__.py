15c2c077ed9ef89851993c7d81b2dc7b32f9ea7b
Manuel Schrauth <manuel.schrauth@live.de>
2022-11-28 18:22:22 +0100

    update version number

--- hypertiling/__init__.py
+++ hypertiling/__init__.py
@@ -2,7 +2,7 @@ from .core import HyperbolicTiling
 
 __all__ = ['HyperbolicTiling']
 
-__version__ = "1.0.2"
+__version__ = "1.1.2"
 __author__ = 'Manuel Schrauth, Felix Dusel, Florian Goth, Dietmar Herdt, Jefferson S. E. Portela, Yanick Thurn'
 __credits__ = 'Institute for Theoretical Physics and Astrophysics, University of Wuerzburg'
 __packages__ = []
