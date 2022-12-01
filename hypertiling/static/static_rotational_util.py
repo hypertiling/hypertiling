0d5a03ba915b33d938b2e6d17267299e2d3b5fad
Manuel Schrauth <manuel.schrauth@live.de>
2022-12-01 11:38:02 +0100

    add warning

--- hypertiling/static/static_rotational_util.py
+++ hypertiling/static/static_rotational_util.py
@@ -2,6 +2,10 @@ import numpy as np
 
 class DuplicateContainerSimple:
     # since set is a hashed type, we need to round
+    # warning: due to the rounding this duplicate container 
+    # is not 100 percent reliable; if can happen that two duplicates
+    # end up being rounded to different numbers even though they are 
+    # only eps apart; in this case they are undetected as duplicates
 
     def __init__(self, digits):
         self.digits = digits
