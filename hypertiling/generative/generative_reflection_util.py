27acc17294cf82e9e5a3c47b5208c3b65877c711
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2022-12-01 17:02:35 +0100

    fix typo

--- hypertiling/generative/generative_reflection_util.py
+++ hypertiling/generative/generative_reflection_util.py
@@ -204,7 +204,7 @@ def generate(p: int, q: int, n: int, r: float, sector_polys: np.array, sector_le
     for j, poly in enumerate(sector_polys[:-1]):  # m/p loop executions
         if reflection_levels[j] == n:
             # all reflection layers are constructed
-            print(f"Created {c}/{stop}")
+            print("Created {c}/{stop}")
             return reflection_levels[:c]
 
         if j > 1:
