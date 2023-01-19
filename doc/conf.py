715d6fba677067efa1f224ce263871269f79016d
J. Stafusa <jstafusa@physik.uni-wuerzburg.de>
2023-01-19 23:37:19 +0100

    Undoing previous attempts to improve the changelog rendering due to dependencies conflicts.

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ author = 'Manuel Schrauth, Felix Dusel, Dietmar Herdt, Florian Goth, Jefferson S
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx', 'sphinx.ext.mathjax', 'sphinx_mdinclude', 'm2r2']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx', 'sphinx.ext.mathjax', 'sphinx_mdinclude']
 nbsphinx_execute = 'never'
 nbsphinx_allow_errors = True
 source_suffix = ['.rst', '.md']
