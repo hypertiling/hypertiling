c5254ef0019dcfbbe9a44ad9b1f49f96adb80d36
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2023-01-22 15:13:41 +0100

    prepare for first run with sandbox

--- assets/zenodo.sh
+++ assets/zenodo.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-pip install cffconvert
+pip install cffconvert gitlab2zenodo
 # The next line does not work fully as of today:
-# $HOME/.local/bin/cffconvert -i ../CITATION.cff -f zenodo -o zenodo.json
+# $HOME/.local/bin/cffconvert -i ../CITATION.cff -f zenodo -o .zenodo.json
