07e11ef476a3d83d53bde3dda462d6213f0c106c
Manuel Schrauth <manuel.schrauth@live.de>
2023-01-25 18:15:39 +0100

    fix bug

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license='MIT',
     keywords='hyperbolic tessellation tiling curvature poincare geometry',
     packages=[package_name,
-              f"{package_name}.kernels",
+              f"{package_name}.kernel",
               f"{package_name}.experimental",
               f"{package_name}.graphics"],
     classifiers=[
