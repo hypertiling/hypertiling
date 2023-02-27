e239bedf706d91d6779d1f94cf482942d1e3d9a6
Yanick Thurn <50019628+YanickT@users.noreply.github.com>
2023-02-27 10:42:41 +0100

    BUGFIX GRG
    BUG also fixed in GRG

--- hypertiling/kernel/GRG.py
+++ hypertiling/kernel/GRG.py
@@ -3,7 +3,7 @@ import numpy as np
 import hypertiling.kernel.GR_util as util
 import hypertiling.kernel.GRG_util as graph_util
 from hypertiling.kernel_abc import Graph
-from ..ion import htprint
+from hypertiling.ion import htprint
 
 """
 p: Number of edges/vertices of a polygon
@@ -245,7 +245,7 @@ if __name__ == "__main__":
     import hypertiling.core as core
     from hypertiling.kernel_abc import Tiling
 
-    p, q, n = 7, 3, 6
+    p, q, n = 3, 8, 9
     n2 = 3
     t1 = time.time()
     graph = GenerativeReflectionGraph(p, q, n)
@@ -262,12 +262,12 @@ if __name__ == "__main__":
     fig_ax[1].set_ylim(-1, 1)
     fig_ax[1].set_box_aspect(1)
     graph.check_integrity()
-    for i, nbrs in enumerate(graph.get_nbrs_list()):
-        print(nbrs)
-        if not np.array_equal(nbrs, graph[i]):
-            print(nbrs)
-            print(graph[i])
-            print("\n")
+    #for i, nbrs in enumerate(graph.get_nbrs_list()):
+    #    print(nbrs)
+    #    if not np.array_equal(nbrs, graph[i]):
+    #        print(nbrs)
+    #        print(graph[i])
+    #        print("\n")
 
     colors = ["#FF000080", "#00FF0080", "#0000FF80"]
     """# tiling.map_layers()
