85f2e095dc02029c0612e19e0dd1a77b87825b5a
Manuel Schrauth <manuel.schrauth@live.de>
2023-03-18 10:03:25 +0100

    improve according to DUN09 notes

--- hypertiling/kernel/DUNP.py
+++ hypertiling/kernel/DUNP.py
@@ -5,27 +5,37 @@ from .SR_base import KernelStaticBase
 from .DUN_util import transformW_poly, transformW_site
 
 
-class DunTrafo:
+# a transformation matrix with some extra information
+class DunhamTransformation:
+    """
+    Transformations contain a matrix, the orientation, 
+    and an index, pPosition, of the edge across which 
+    the last transformation was made
+    """
     def __init__(self, matrix, orientation, p_position):
         self.matrix = matrix
         self.orientation = orientation
         self.p_position = p_position
 
-    def __mult__(self, other):
-        return self.matrix @ other.matrix
-        
+    def __mul__(self, other):
+        new_matrix = self.matrix @ other.matrix
+        new_orient = self.orientation * other.orientation
+        new_p_pos  = other.p_position
+
+        return DunhamTransformation(new_matrix, new_orient, new_p_pos)
+
+
 
 
 class LegacyDunhamPlus(KernelStaticBase):
     """
-    This kernel implements the "original" construction algorithm of D. Dunham (1982)
-    The algorithm uses Weierstraß (hyperboloid) coordinates; since those are not natively supported
-    by our HyperPolygon class we need transformation functions provided in DUN_util.py
     """
 
     def __init__ (self, p, q, n, **kwargs):
         super(LegacyDunhamPlus, self).__init__(p, q, n, **kwargs)
 
+        if p==3 or q==3:
+            htprint("Warning", "p=3 or q=3 not supported")
 
         if self.center == "vertex":
             htprint("Warning", "Dunham kernel does not support vertex centered tilings yet!")
@@ -47,30 +57,36 @@ class LegacyDunhamPlus(KernelStaticBase):
         self.RotQ  = self.ReflectPgonEdge @ self.ReflectHypotenuse
         self.Rot2P = self.RotP @ self.RotP
         self.Rot3P = self.Rot2P @ self.RotP
+        self.Rot4P = self.Rot3P @ self.RotP
+        self.Rot5P = self.Rot4P @ self.RotP
+        self.Rot6P = self.Rot5P @ self.RotP
+        self.Rot7P = self.Rot6P @ self.RotP
+        self.Rot8P = self.Rot7P @ self.RotP
+
         self.RotCenterG = np.eye(3)     # will be manipulated in self.generate()
         self.RotCenterR = np.eye(3)     # will be manipulated in self.replicate()
+
+
+        # We define the exposure of a p-gon in terms of the number of edges 
+        # it has in common with the next layer.
+        # A p-gon has minimum exposure if it has the fewest edges in common with 
+        # the next layer, and thus shares an edge with the previous layer.
+        # A p-gon has maximum exposure if it has the most edges in common with the 
+        # next layer, and thus only shares a vertex with the previous layer.
+        # We abbreviate these values as min_exp and max_exp, respectively.
 
         self.max_exp = self.p - 2
         self.min_exp = self.p - 3
 
-
-        self.vertex_valences = self.q * np.ones(self.p).astype("int")
-
-
-        self.edge_tran = [self.ReflectPgonEdge,
-                          self.ReflectPgonEdge@self.RotP, 
-                          self.ReflectPgonEdge@self.Rot2P,
-                          self.ReflectPgonEdge@self.Rot3P,
-                          self.ReflectPgonEdge@self.Rot3P@self.RotP,
-                          self.ReflectPgonEdge@self.Rot3P@self.Rot2P,
-                          self.ReflectPgonEdge@self.Rot3P@self.Rot3P]
+        # hard coded for (7,X) tiling!!!!!!
+        self.edge_tran = [DunhamTransformation(self.ReflectPgonEdge,            1, 0),
+                          DunhamTransformation(self.ReflectPgonEdge@self.RotP,  1, 1),
+                          DunhamTransformation(self.ReflectPgonEdge@self.Rot2P, 1, 2),
+                          DunhamTransformation(self.ReflectPgonEdge@self.Rot3P, 1, 3),
+                          DunhamTransformation(self.ReflectPgonEdge@self.Rot4P, 1, 4),
+                          DunhamTransformation(self.ReflectPgonEdge@self.Rot5P, 1, 5),
+                          DunhamTransformation(self.ReflectPgonEdge@self.Rot6P, 1, 6)]
         
-
-        self.pShiftArray = [1, 0]
-        self.verticesToSkipArray = [3, 2]
-        self.qShiftArray = [0, -1]
-        self.polygonsToSkipArray = [2, 3]
-        self.exposureArray = [self.max_exp, self.min_exp]
 
         # fundamental polygon of the tiling
         self._create_first_layer(self.phi/2)
@@ -79,6 +95,7 @@ class LegacyDunhamPlus(KernelStaticBase):
         self._generate()
 
 
+    # add polygon to tiling
     def _draw_pgon_pattern(self, trans):
         # create permanent copy of fundamental polygon
         poly = copy.deepcopy(self.fund_poly)
@@ -88,48 +105,44 @@ class LegacyDunhamPlus(KernelStaticBase):
         self.polygons.append(poly)
 
 
-
+    # increment transformation
     def add_to_tran(self, tran, shift):
         if shift % self.p == 0:
             return tran
         else:
             return self.compute_tran(tran, shift)
-    
 
     def compute_tran(self, tran, shift):
         newEdge = (tran.p_position + tran.orientation * shift) % self.p
         return tran*self.edge_tran[newEdge]
 
 
-
+    # central recursion
     def replicate_motif(self, poly, initialTran, layer, exposure):
+        
+        # draw polygon
         self._draw_pgon_pattern(initialTran)
 
-        if layer <= self.n:
+        # proceed to desired depth
+        if layer < self.n:
             #  Which vertex to start at
-            pShift = self.pShiftArray[exposure]
-            verticesToDo = self.p - self.verticesToSkipArray[exposure]
+            min_exposure = (exposure == self.min_exp)
+            pShift = 1 if min_exposure else 0
+            verticesToDo = self.p-3 if min_exposure else self.p-2
 
             # Iterate over vertices
             for i in range(1, verticesToDo+1):
-                pTran = self.compute_tran(initialTran, pShift)
                 first_i = (i==1)
-                qTran = self.add_to_tran(initialTran, pShift[first_i])
-
-                if (pTran.orientation > 0):
-                    vertex = (pTran.p_position-1) % self.p
-                else: 
-                    vertex = pTran.p_position
-
-                polygonsToDo = self.vertex_valences[vertex] - self.polygonsToSkipArray[first_i]
+                pTran = self.compute_tran(initialTran, pShift)
+                qSkip = -1 if first_i else 0
+                qTran = self.add_to_tran(pTran, qSkip)
+                pgonsToDo = self.p-3 if first_i else self.p-2
 
                 # Iterate about a vertex
-                for j in range(1, polygonsToDo):
+                for j in range(1, pgonsToDo):
                     first_j = (j==1)
-                    newExposure = self.exposureArray[first_j]
-
+                    newExposure = self.min_exp if first_j else self.max_exp
                     self.replicate_motif(poly, qTran, layer+1, newExposure)
-
                     qTran = self.add_to_tran(qTran, -1)
 
                 # Advance to next vertex
@@ -137,14 +150,21 @@ class LegacyDunhamPlus(KernelStaticBase):
             
 
 
+    # top-level driver routine
     def replicate(self, poly):
-        qtran = DunTrafo(self.edge_tran[1], 0, 1)
+        # assume the fundamental polygon itself has already been
+        # added to the tiling by the init method
         
-        
+        # Iterate over each vertex
+        for i in range(1, self.p+1):
+            qtran = self.edge_tran[i-1]
 
-        for j in range(0, self.vertex_valences[1]):
-            self.replicate_motif(poly, qtran, 2, self.max_exp)
-            qtran = self.add_to_tran(qtran, -1)
+            # Iterate about a vertex
+            for j in range(1, self.q-2+1):
+                exposure = self.min_exp if (j==1) else self.max_exp
+                self.replicate_motif(poly, qtran, 2, exposure)
+                qtran = self.add_to_tran(qtran, -1)
+
 
 
     def _generate(self):
