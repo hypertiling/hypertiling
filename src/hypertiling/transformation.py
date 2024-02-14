8b2a52811cc3760703152c233570af03d71e013d
Manuel Schrauth <manuel.schrauth@live.de>
2024-02-14 09:20:16 +0100

    add proper doc strings in transformation module

--- src/hypertiling/transformation.py
+++ src/hypertiling/transformation.py
@@ -11,7 +11,19 @@ def moeb_rotate_trafo(phi, z):
 
 @NumbaChecker(["UniTuple(complex128, 2)(complex128, complex128)"])
 def mymoebint(z0, z):
-    '''Internal function for performing a full Möbius transform in double-double representation.'''
+    """
+    Internal function for performing a full Möbius transform in double-double representation.
+
+    Parameters
+    ----------
+    z0 : complex
+    z : complex
+
+    Returns
+    -------
+    ret : complex
+    dret : complex
+    """
     dz0 = complex(0, 0)
     dz = complex(0, 0)
     one = complex(1, 0)
@@ -26,14 +38,18 @@ def mymoebint(z0, z):
 def moeb_origin_trafo(z0, z):
     """
     Maps all points z such that z0 -> 0, respecting the Poincare projection: (z - z0)/(1 - z0 * z)
-    
-    Parameters:
-        z0 (complex): the origin that we map back to.
-        z (complex): the point that we will tr
 
-    
-    Returns:
-        ret (complex): z Möbius transformed around z0: (z - z0)/(1 - z0 * z)
+    Parameters
+    ----------
+    z0 : complex
+        The origin that we map back to.
+    z : complex
+        The point that we will tr
+
+    Returns
+    -------
+    ret : complex
+        z Möbius transformed around z0: (z - z0)/(1 - z0 * z)
     """
     ret, dret = mymoebint(-z0, z)
     return ret
@@ -41,7 +57,21 @@ def moeb_origin_trafo(z0, z):
 
 @NumbaChecker(["UniTuple(complex128, 2)(complex128, complex128, complex128, complex128)"])
 def moeb_origin_trafodd(z0, dz0, z, dz):
-    '''Möbius transform to the origin in double double representation'''
+    """
+    Möbius transform to the origin in double double representation.
+
+    Parameters
+    ----------
+    z0 : complex
+    dz0 : complex
+    z : complex
+    dz : complex
+
+    Returns
+    -------
+    tuple of complex
+        The transformed complex numbers (ret, dret)
+    """
     one = complex(1, 0)
     done = complex(0, 0)
     nom, dnom = htcplxdiff(z, dz, z0, dz0)
@@ -53,9 +83,22 @@ def moeb_origin_trafodd(z0, dz0, z, dz):
 
 @NumbaChecker(["UniTuple(complex128, 2)(complex128, complex128, float64)"])
 def moeb_rotate_trafodd(z, dz, phi):
-    '''Rotation of a complex number'''
+    """
+    Rotation of a complex number.
+
+    Parameters
+    ----------
+    z : complex
+    dz : complex
+    phi : float
+
+    Returns
+    -------
+    tuple of complex
+        The rotated complex numbers (ret, dret)
+    """
     ep = complex(math.cos(phi), math.sin(phi))
-    ep = ep / abs(ep)  # We calculated sin and cos separately. We can't be sure that |ep| == 1
+    ep = ep / abs(ep)
     dep = complex(0, 0)
     ret, dret = htcplxprod(z, dz, ep, dep)
     return ret, dret
@@ -63,7 +106,21 @@ def moeb_rotate_trafodd(z, dz, phi):
 
 @NumbaChecker(["UniTuple(complex128, 2)(complex128, complex128, complex128, complex128)"])
 def moeb_origin_trafo_inversedd(z0, dz0, z, dz):
-    '''Inverse Möbius transform to the origin in double double representation'''
+    """
+    Inverse Möbius transform to the origin in double double representation.
+
+    Parameters
+    ----------
+    z0 : complex
+    dz0 : complex
+    z : complex
+    dz : complex
+
+    Returns
+    -------
+    tuple of complex
+        The inverse transformed complex numbers (ret, dret)
+    """
     one = complex(1, 0)
     done = complex(0, 0)
     nom, dnom = htcplxadd(z, dz, z0, dz0)
