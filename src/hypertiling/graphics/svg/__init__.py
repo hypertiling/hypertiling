3fe43ceb31e71fa174674c67876d0efb1373c039
Manuel Schrauth <manuel.schrauth@live.de>
2025-10-29 11:28:09 +0100

    provide svg module init file for simpler import; move check_hyperbolic to not affect triangle tilings

---
+++ src/hypertiling/graphics/svg/__init__.py
@@ -0,0 +1,43 @@
+"""
+SVG drawing utilities for hyperbolic tilings in the Poincaré disk.
+
+This module provides object-oriented SVG elements and a canvas for rendering
+hyperbolic geometric objects.
+
+Examples
+--------
+>>> from hypertiling.graphics.svg import SvgCanvas, Tiling, Geodesic, UnitCircle
+>>> from hypertiling import HyperbolicTiling
+>>> 
+>>> T = HyperbolicTiling(5, 4, 5)
+>>> canvas = SvgCanvas(width=600, height=600)
+>>> canvas.add(UnitCircle())
+>>> canvas.add(Tiling(T, skip_first=True))
+>>> display(canvas)
+"""
+
+from .svg_base import SvgElement, SvgCanvas, display, build_svg_attrs
+from .tiling import Tiling, Polygon
+from .circle import HyperbolicCircle, UnitCircle
+from .geodesic import Geodesic
+from .horocycle import Horocycle
+from .hypercycle import Hypercycle
+
+__all__ = [
+    # Base classes
+    'SvgElement',
+    'SvgCanvas',
+    'display',
+    
+    # Geometric elements
+    'Polygon',
+    'Tiling',
+    'Geodesic',
+    'HyperbolicCircle',
+    'Horocycle',
+    'Hypercycle',
+    'UnitCircle',
+    
+    # Utilities
+    'build_svg_attrs',
+]
