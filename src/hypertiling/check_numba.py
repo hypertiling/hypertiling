418ce52032700542db4495ac9d706843875d3a12
Yanick T. <50019628+YanickT@users.noreply.github.com>
2026-05-18 10:29:17 +0200

    fix GRGS
    FIX: .fill(-1) changed to max value of array (on purpose underflow removed)

--- src/hypertiling/check_numba.py
+++ src/hypertiling/check_numba.py
@@ -3,7 +3,7 @@ import warnings
 
 try:
     from numba import njit
-    # raise ImportError("dummy") # manually block numba
+    raise ImportError("dummy") # manually block numba
 
     AVAILABLE = True
 except Exception as error:
